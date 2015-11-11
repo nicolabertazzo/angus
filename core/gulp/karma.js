99cb3c5421655d0a6df0c626891f934b638a22ea
traycold <traycold@users.noreply.github.com>
2015-11-11 16:02:12 +0100

    Inject js files into karma.conf.js; use karma.conf.js file
    Inject both bower components and application js files into karma.conf.js.
    Make use of karma.conf.js when launching Karma.

--- core/gulp/karma.js
+++ core/gulp/karma.js
@@ -1,19 +1,55 @@
 'use strict';
 
-var karma = require('karma').server;
 
-module.exports = function (angus) {
+var replace = require('gulp-replace');
+var path = require('path');
+var angularFilesort = require('gulp-angular-filesort');
+var inject = require('gulp-inject');
+var Server = require('karma').Server;
+
+module.exports = function (angus, gulp) {
+	
+	var autoInclude = {
+        jsLib: '',
+		jsApp: ''
+    };	
+	
+	autoInclude.jsLib = angus.appConfig.bower.filesNeeded.js
+	.map(function (filePath) {
+		filePath = 'bower_components/' + filePath;
+		return '"' + filePath + '",';
+	}).join('\n    ');
+   
+  var basesubstr = angus.appPath.length+1;
+  var filenames = "";
+  var appStream = gulp.src(angus.appPath +'/src/core/**/*.js');
+	
     return function (done) {
-        karma.start({
-            browsers: ['PhantomJS'],
-            files: [ angus.appPath + '/tests/**/*.js'],
-            frameworks: ['jasmine'],
+		gulp.src(angus.appPath + '/template/karma.conf.js')
+            .pipe(replace(/<!-- autoInclude: jsLib !-->/g, autoInclude.jsLib))
+			.pipe(inject(angus.appConfig.usesAngularJS?appStream.pipe(angularFilesort()):appStream, {
+				starttag: '//karmabegin',
+				endtag: '//karmaend',
+				transform: function (filepath, file, i, length) {
+					//console.log('  "' + file.path.substr(basesubstr).replace(/\\/g, '\/') + '"' + (i + 1 < length ? ',' : ''));
+					if(file.path.substr(basesubstr).replace(/\\/g, '\/').indexOf('/app.js') > -1) {
+						return '  \'tests/app.js' + '\'' + (i + 1 < length ? ',' : '');
+					}
+				  return '  \'' + file.path.substr(basesubstr).replace(/\\/g, '\/') + '\'' + (i + 1 < length ? ',' : '');
+				}
+			}))
+			//.pipe(replace(/@@minified/g, angus.env === 'dev' ? '' : '.min'))
+			.pipe(gulp.dest(angus.appPath + '/'));
+			
+		new Server({			
+            configFile: angus.appPath + '/karma.conf.js',
             singleRun: true
-        }, function (exitCode) {
+		  }, function (exitCode) {
             console.log('Karma has exited with ' + exitCode);
             if (exitCode === 0) {
                 done();
             }
-        });
+        }).start();
+		  
     };
 };
